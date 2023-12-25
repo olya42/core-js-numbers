55d43fe5f961f95f5ad605e0de730d9c7f34ec75
olya <olyashlykova7@gmail.com>
2023-12-26 00:41:24 +0600

    fix: getSunDigits  function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -300,7 +300,7 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(num) {
+function getSumOfDigits(/* num */) {
   throw new Error('Not implemented');
 }
 
